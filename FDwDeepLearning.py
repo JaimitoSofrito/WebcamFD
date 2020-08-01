84a2545140bbffed044c35d9f494759082a98a96
Jaime Gonzalez <jaime.gonzalez5@upr.edu>
2020-08-01 16:19:11 -0400

    First

---
+++ FDwDeepLearning.py
@@ -0,0 +1,35 @@
+# face detection with Multi-Task Cascaded Convolutional Neural Network on a photograph
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from mtcnn.mtcnn import MTCNN
+
+
+
+# draw an image with detected objects
+def draw_image_with_boxes(filename, result_list):
+    data = pyplot.imread(filename)
+    pyplot.imshow(data)
+    ax = pyplot.gca()
+    for result in result_list:
+        x, y, width, height = result['box']
+        rect = Rectangle((x, y), width, height, fill=False, color='red')
+        ax.add_patch(rect)
+        # draw the dots
+        for key, value in result['keypoints'].items():
+            # create and draw dot
+            dot = Circle(value, radius=2, color='red')
+            ax.add_patch(dot)
+    # show the plot
+    pyplot.show()
+
+
+filename = 'Untitledw.png'
+# load image from file
+pixels = pyplot.imread(filename)
+# create the detector, using default weights
+detector = MTCNN()
+# detect faces in the image
+faces = detector.detect_faces(pixels)
+# display faces on the original image
+draw_image_with_boxes(filename, faces)
